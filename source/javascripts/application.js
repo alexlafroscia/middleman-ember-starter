7547c56712283e63461cec5da5d579def829907b
Alex LaFroscia <alex.lafroscia@gmail.com>
2014-02-20 16:43:09 -0500

    Moved application.js and router.js to Coffeescript

--- source/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require 'jquery/jquery.min'
-//= require 'handlebars/handlebars'
-//= require 'ember/ember'
-//= require 'ember-data/ember-data'
-//= require ./main
---
+++ source/javascripts/application.js.coffee
@@ -0,0 +1,5 @@
+#= require 'jquery/jquery.min'
+#= require 'handlebars/handlebars'
+#= require 'ember/ember'
+#= require 'ember-data/ember-data'
+#= require ./main
