656a7897004d03451084b7a8c0b0fadd832a551a
Alex LaFroscia <alex.lafroscia@gmail.com>
2014-02-20 16:40:32 -0500

    Moved main.js to coffeescript

--- source/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-//= require_self
-//= require_tree ./controllers
-//= require_tree ./models
-//= require_tree ./routes
-//= require_tree ./templates
-//= require_tree ./views
-
-window.App = Ember.Application.create();
